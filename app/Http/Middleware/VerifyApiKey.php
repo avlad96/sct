2c214410383635230db4f21629b089c458cd2255
avlad96 <anvlad96@gmail.com>
2025-10-01 11:50:57 +0300

    feat: configure API routes and middleware

---
+++ app/Http/Middleware/VerifyApiKey.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyApiKey
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->header('X-API-Key') !== config('app.api_key')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
