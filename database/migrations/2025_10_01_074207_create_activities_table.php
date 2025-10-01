91a1ff1b7399fa274061add9a4ed2464b907098a
avlad96 <anvlad96@gmail.com>
2025-10-01 12:17:55 +0300

    fix: make parent_id nullable in activities table

--- database/migrations/2025_10_01_074207_create_activities_table.php
+++ database/migrations/2025_10_01_074207_create_activities_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('parent_id')->constrained('activities')->cascadeOnDelete();
+            $table->foreignId('parent_id')->nullable()->constrained('activities')->cascadeOnDelete();
             $table->timestamps();
         });
     }
